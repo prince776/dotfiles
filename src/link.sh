e39f31e5711a637e2eedf17281333f98ef17c785
Prince Gupta <21282111+prince776@users.noreply.github.com>
2024-02-17 20:36:24 +0530

    Link gitconfig

--- src/link.sh
+++ src/link.sh
@@ -11,3 +11,10 @@ if [ -d $configDir ]; then
     echo yes | rm -rf $configDir
 fi
 ln -s "$PWD/config/" $configDir
+
+gitconfig=~/.gitconfig
+if [ -f $gitconfig ]; then
+    echo "Skipping linking the gitconfig file, (re)move it to link it"
+fi
+
+ln -s "$PWD/.gitconfig" $gitconfig
